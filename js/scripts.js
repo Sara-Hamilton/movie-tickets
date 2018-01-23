b697e144acf2193b08671b66ab133b7a57d6043c
Mariah Shipley and Sara Hamilton <mariah@>
2018-01-23 11:11:58 -0800

    add price calculating function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,44 @@
+// business logic
+var inputtedMovieName = ("");
+var inputtedMovieTime = ("");
+var inputtedMovieType = ("");
+var inputtedQuantity = 0;
+var price = 0;
+
 function Ticket(name, time, age, cost, firstRelease) {
   this.name = name;
   this.time = time;
-  this.age = age;
+  this.type = type;
   this.cost = cost;
   this.firstRelease = false;
 }
+
+price = function(time, type, quantity) {
+  var timePrice = 0;
+  var typePriceAdjustment = 0;
+  if (time === "Matinee (before your teenagers wake up)") {
+    timePrice = 9;
+  } else {
+    timePrice = 12;
+  }
+  if (type === "Adult") {
+    typePriceAdjustment = 1;
+  } else {
+    typePriceAdjustment = 0.6;
+  }
+  return parseFloat((timePrice * typePriceAdjustment * quantity).toFixed(2));
+}
+
+// user interface logic
+$(document).ready(function(){
+  $("form#new-movie").submit(function(event){
+    event.preventDefault();
+
+    inputtedMovieName = $("select#new-movie-name").val();
+    inputtedMovieTime = $("select#new-time").val();
+    inputtedMovieType = $("select#new-type").val();
+    inputtedQuantity = parseInt($("input#new-quantity").val());
+    price = price(inputtedMovieTime, inputtedMovieType, inputtedQuantity).toFixed(2);
+    console.log("price "+ price);
+  });
+});
